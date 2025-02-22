e5b9658794fac2173d9a2747440d5b1be22c164c
Yohei WASHIZAKI <yohei.washizaki@gmail.com>
2025-02-22 22:33:01 +0900

    ロケール一覧の拡張

---
+++ cmd/locale.go
@@ -0,0 +1,82 @@
+/*
+Copyright © 2025 Yohei WASHIZAKI <yohei.washizaki@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// localeCmd represents the locale command
+var localeCmd = &cobra.Command{
+	Use:   "locale",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		locale := viper.GetString("locale")
+		fmt.Printf("%s\n", locale)
+	},
+}
+
+type Locale struct {
+	Code		string
+	DisplayName string
+}
+
+var SupportedLocalesMap = map[string]Locale{
+	"JaJP": {Code: "JaJP", DisplayName: "日本語"},
+	"EnUS": {Code: "EnUS", DisplayName: "English(US)"},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, loc := range SupportedLocalesMap{
+			fmt.Printf("%s\n", loc.Code)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(localeCmd)
+	localeCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// localeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// localeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
